081afd86f799b32e9c2979e3539c91d6d5daaa78
Cristi-Alex <cristi.zacoi@yahoo.com>
2017-06-24 16:07:59 +0300

    config files changes

--- web.js
+++ web.js
@@ -1,11 +1,7 @@
 /**
  * Created by Cris on 24.06.2017.
  */
-var express = require('express'),
-    app = express();
-
-app.use(express.static(__dirname));
-app.get('/', function(req, res) {
-    res.sendfile('index.html', {root: __dirname })
-});
-var server = app.listen(process.env.PORT || 80);
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 3000);

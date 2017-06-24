04b18366f9f967e0fbab4a0950846ae900b49ef3
Cristi-Alex <cristi.zacoi@yahoo.com>
2017-06-24 15:44:11 +0300

    added config files

---
+++ web.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Cris on 24.06.2017.
+ */
+var express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname })
+});
+var server = app.listen(process.env.PORT || 80);

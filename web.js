b4c2f3839b424d0b524e8abdff81b1defcff103c
Cristi-Alex <cristi.zacoi@yahoo.com>
2017-06-26 02:51:34 +0300

    heroku

--- web.js
+++ web.js
@@ -3,10 +3,12 @@
  */
 var express = require('express');
 var app = express();
+
 app.use(express.static(__dirname + '/'));
 app.get('/', function(req, res) {
     res.sendfile('index.html', {root: __dirname })
 });
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var port_number = process.env.PORT || 3000;
+app.listen(port_number, function(){
+    console.log("Express server listening on port %d in %s mode", port_number, app.settings.env);
 });
